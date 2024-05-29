98f36f1be7497fee37fc19f7c8aab0d353ec3bcb
PJazminCastro <125835937+PJazminCastro@users.noreply.github.com>
2024-05-28 20:29:34 -0600

    Funciones

---
+++ ReactNative/JSReact/index.js
@@ -0,0 +1,24 @@
+console.log('Hola consola');
+/*
+// Funciones
+// Tradicional
+function suma(a,b){
+    return a+b;
+}
+console.log(suma(1,10));
+
+// Fat arrow function 
+const resta = (a, b) =>  { return a-b }
+console.log(resta(100,10));
+
+// Fat arrow simplificada
+const restas = (a, b) =>  a-b;
+console.log(restas(100,11)); */
+
+// Trabajo con objetos
+const usuario = {
+    nombre: 'Ana',
+    apellido: 'Guerra',
+    edad: 30,
+    direccion: 
+};
