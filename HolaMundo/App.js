3db77ad12fd122af80ff2c59a122c3d48e2d7189
PJazminCastro <125835937+PJazminCastro@users.noreply.github.com>
2024-06-18 20:23:40 -0600

    Propiedades y estados

--- HolaMundo/App.js
+++ HolaMundo/App.js
@@ -1,14 +1,30 @@
 // Importaciones
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import React, { useState } from 'react';
 
-// Componentes
+// Componente propio
+const Texto = () => {
+  const [contenido, setcontenido] = useState('Hola :)')
+  const actualizarTexto = () => { setcontenido('Adiós :(') }
+  return (
+    <Text onPress={actualizarTexto}> {contenido} </Text>
+  )
+}
+// Ejecución de proyecto
 export default function App() {
   return (
     // Vista (componente padre)
     <View style={styles.container}>
-      {/* Texto */}
-      <Text>Hola mundo ReactNative</Text>
+      {/* Texto
+      <Text>Hola mundo ReactNative</Text> */}
+
+      {/* Llamar componente de "texto" */}
+      <Texto>  </Texto>
+      <Texto>  </Texto>
+
+      <Button title='Saludar' color={'#f194ff'}>  </Button>
+
       {/* Estatus bar, solo en teléfono */}
       <StatusBar style="auto" />
     </View>
